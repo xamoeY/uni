e006d2ea2cf12b727bb7f2e075701cbbcc508f4f
Sven-Hendrik Haase <sh@lutzhaase.com>
2012-10-24 13:10:38 +0200

    Uebung 2, simple ist schon gefixt

---
+++ hlr/02-gdb-valgrind/simple/simple.c
@@ -0,0 +1,69 @@
+/* 
+** simple err-demonstration to demonstrate power of valgrind 
+** Julian M. Kunkel - 17.04.2008
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// dynamic
+int *
+mistake1 ()
+{
+  int* buf = malloc (sizeof (int) * 6);
+  buf[0] = 1;
+  buf[1] = 1;
+  buf[2] = 2;
+  buf[3] = 3;
+  buf[4] = 4;
+  buf[5] = 5;
+  return buf;
+}
+
+// dynamic
+int *
+mistake2 ()
+{
+  int *buf = malloc (sizeof (char) * 8);
+  buf[1] = 2;
+  return buf;
+}
+
+// static
+int *
+mistake3 ()
+{
+  int *buf = mistake1();
+  buf[0] = 3;
+  return buf;
+}
+
+// dynamic
+int *
+mistake4 ()
+{
+  int *buf = malloc (sizeof (char) * 4);
+  buf[0] = 4;
+  return buf;
+}
+
+int
+main (void)
+{
+  /* Modifizieren Sie die folgenden Zeilen nicht */
+  int *p[4] = { &mistake1 ()[1], &mistake2 ()[1], mistake3 (), mistake4 () };
+
+  printf ("1 %d\n", *p[0]);
+  printf ("2 %d\n", *p[1]);
+  printf ("3 %d\n", *p[2]);
+  printf ("4 %d\n", *p[3]);
+
+  /* mhh muss hier noch etwas gefreed werden? */
+  /* Fügen sie hier die korrekten aufrufe von free() ein */
+  free (p[1]-1);			/* welcher Pointer war das doch gleich?, TODO: Fixme... :-) */
+  free (p[0]-1);
+  free (p[3]);
+  free (p[2]);
+
+  return 0;
+}
