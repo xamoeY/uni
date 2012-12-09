d7fd8be256eecfcc607099df2cb7484d759f958a
ElenaBB <ElenaNoll1990@gmail.com>
2012-12-09 18:04:05 +0100

    Blatt6 weiter bearbeitet

--- se3-fp/Blatt6_SvenHendrikHaase_ElenaNoll.rkt
+++ se3-fp/Blatt6_SvenHendrikHaase_ElenaNoll.rkt
@@ -5,21 +5,28 @@
 ;Aufgabe 1
 
 ;Aufgabe 2
+(define bilderrahmen (empty-scene 500 300))
 
 (define schneemann (above/align
                     "center"
                     ;Hut
                     (rectangle 15 20 "solid" "black")
                     (ellipse 40 3 "solid" "black")
-                    ;Der Körper
+                    ;Kopf
                     (underlay/xy
                      (underlay/xy
                       (underlay/xy (ellipse 40 40 "solid" "gray")
                                    7 12
                                    (ellipse 10 10 "solid" "black"))
-                      10 25
-                      (triangle/ssa 32 52 7  "solid" "orange"))
-                     23 12
+                      23 12
                      (ellipse 10 10 "solid" "black"))
+                     10 25
+                     (triangle/ssa 32 52 7  "solid" "orange"))
+                    ;Körper
                     (ellipse 80 70 "solid" "gray")
                     (ellipse 110 100 "solid" "gray")))
+
+(define erzeuge-bild (underlay/xy
+                      bilderrahmen
+                      0 0
+                      schneemann))
