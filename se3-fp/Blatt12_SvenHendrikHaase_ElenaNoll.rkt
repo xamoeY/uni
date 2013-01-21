d66bf6d10afd8107b8a484a5f3e88307861bfd1a
ElenaBB <ElenaNoll1990@gmail.com>
2013-01-21 09:51:41 +0100

    Aufgabe 1 fertig

--- se3-fp/Blatt12_SvenHendrikHaase_ElenaNoll.rkt
+++ se3-fp/Blatt12_SvenHendrikHaase_ElenaNoll.rkt
@@ -9,7 +9,10 @@
       (+ (* (expt 10 zaehler) (car xs)) (rek-zahl (cdr xs) (+ zaehler 1)))))
 
 ;1.2
-
+(define (hoehere-zahl xs)
+  (apply + (map (lambda (n1 n2)
+                  (* n1 (expt 10 n2)))
+                xs (build-list (length xs) values))))
 ;Aufgabe 2
 
 ;2.1
