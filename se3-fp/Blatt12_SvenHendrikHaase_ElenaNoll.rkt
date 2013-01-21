a9ec9543e547c00311c3005b12d2af505ea6c7d8
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-01-21 13:05:15 +0100

    SE3 Lösung für 2.2?

--- se3-fp/Blatt12_SvenHendrikHaase_ElenaNoll.rkt
+++ se3-fp/Blatt12_SvenHendrikHaase_ElenaNoll.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require swindle/extra)
+
 ;Aufgabe 1
 
 ;1.1
@@ -17,3 +19,34 @@
 
 ;2.1
 
+;Als fixe Eingabe dienen drei Listen "summand1", "summand2" und "summe".
+;Diese Listen beeinhalten schlicht die Zahlen als Buchstaben repräsentiert.
+;Wir lösen dieses Problem mittels amb. Es wird dadurch durch alle Möglichkeiten hindurch getestet,
+;ob die Bedingungen erfüllt werden.
+;Die Liste der Eingabebuchstaben hat folgendes Format: '(Buchstabe . (amb 0 1 2 3 4 5 6 7 8 9)
+;Also z.B.: '((A . 0) (B . 4))
+
+;2.2
+
+(define alphabet (string->list "ABCDEFGHIJKLMNOPQRSTUVWXZY"))
+
+(define (distinct? list)
+  (equal? (length list) (length (remove-duplicates list))))
+
+(define (mach-amb-list summand1 summand2 summe)
+  (map
+    (lambda (xs) (cons xs (amb 0 1 2 3 4 5 6 7 8 9)))
+    (remove-duplicates (append summand1 summand2 summe))))
+
+(define (prepare-number-list compare-list1 compare-list2)
+  (reverse (map
+             (lambda (x) (cdr x))
+             (filter (lambda (xs) (not (false? (member (car xs) compare-list1)))) compare-list2))))
+
+(define (krypto summand1 summand2 summe)
+  (let ((buchstaben (mach-amb-list summand1 summand2 summe)))
+    (amb-assert (and (distinct? (map (lambda (xs) (cdr xs)) buchstaben))
+                     (= (+ (hoehere-zahl (prepare-number-list summand1 buchstaben))
+                           (hoehere-zahl (prepare-number-list summand2 buchstaben)))
+                        (hoehere-zahl (prepare-number-list summe buchstaben)))))
+    buchstaben))
