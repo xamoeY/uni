a1e67f8e53dcb81f986a6363e26ce22fb6cfe948
ElenaBB <ElenaNoll1990@gmail.com>
2013-01-28 03:55:11 +0100

    Aufgabe 1.1 fertig

---
+++ se3-fp/Blatt10_SvenHendrikHaase_ElenaNoll.rkt
@@ -0,0 +1,88 @@
+#lang swindle
+
+;Aufgabe 1
+
+;1.1
+(define (erzeuge-key autor jahr titel)
+  (string-append titel autor (number->string jahr)))
+
+(defclass literaturbeitrag ()
+   (key
+   :reader read-key
+   :initarg :init-key)
+  (autor
+   :reader read-autor
+   :initarg :init-autor)
+  (jahr
+   :reader read-jahr
+   :initarg :init-jahr)
+  (titel
+   :reader read-titel
+   :initarg :init-titel))
+
+(defclass buch (literaturbeitrag)
+  (verlag
+   :reader read-verlag
+   :initarg :init-verlag)
+  (verlagsort
+   :reader read-verlagsort
+   :initarg :init-verlagsort)
+  (reihe
+   :reader read-reihe
+   :initarg :init-reihe)
+  (seriennummer
+   :reader read-seriennummer
+   :initarg :init-seriennummer))
+
+(defclass sammelband (buch)
+  (herausgeber
+   :reader read-herausgeber
+   :initarg :init-herausgeber)
+  (seitenangabe
+   :reader read-seitenangabe
+   :initarg :init-seitenangabe))
+
+(defclass zeitschriftenartikel (literaturbeitrag)
+  (zeitschrift
+   :reader read-zeitschrift
+   :initarg :init-zeitschrift)
+  (bandnummer
+   :reader read-bandnummer
+   :initarg :init-bandnummer)
+  (heftnummer
+   :reader read-heftnummer
+   :initarg :init-heftnummer)
+  (erscheinungsmonat
+   :reader read-erscheinungsmonat))
+
+(define Beispiel-Buch (make buch
+                            :init-key (erzeuge-key "Nessie" 1790 "Mein Leben im Loch Ness: Verfolgt als Ungeheuer")
+                            :init-autor "Nessie"
+                            :init-jahr 1790
+                            :init-titel "Mein Leben im Loch Ness: Verfolgt als Ungeheuer"
+                            :init-verlag "Minority-Verlag"
+                            :init-verlagsort "Inverness"
+                            :init-reihe "Die besondere Biographie"
+                            :init-seriennummer "Band 1"))
+
+(define Beispiel-Sammelband (make sammelband
+                                  :init-key (erzeuge-key "Prefect, F." 1979 "Mostly harmless - some observertions concerning the third planet of the solar system")
+                                  :init-autor "Prefect, F."
+                                  :init-jahr 1979
+                                  :init-titel "Mostly harmless - some observertions concerning the third planet of the solar system"
+                                  :init-verlag "Galactic Press"
+                                  :init-verlagsort "Vega-System, 3rd planet"
+                                  :init-reihe "Travel in Style"
+                                  :init-seriennummer "Volume 5"
+                                  :init-herausgeber "Adams, D."
+                                  :init-seitenangabe 500))
+
+(define Beispiel-Zeitschriftenartikel (make zeitschriftenartikel
+                                            :init-key (erzeuge-key "Wells, H. G." 3200 "Zeitmaschinen leicht gemacht")
+                                            :init-autor "Wells, H. G."
+                                            :init-jahr 3200
+                                            :init-titel "Zeitmaschinen leicht gemacht"
+                                            :init-zeitschrift "Heimwerkerpraxis für Anfänger"
+                                            :init-bandnummer 3
+                                            :init-heftnummer 500))
+                                            
