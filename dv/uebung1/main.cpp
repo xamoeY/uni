b8f6479accbf57b6c22980f8e7318a19723454c8
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-05-14 11:44:06 +0200

    Triangle und so

--- dv/uebung1/main.cpp
+++ dv/uebung1/main.cpp
@@ -1,10 +1,58 @@
+// standard C++ libraries
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+#include <cmath>
 #include <iostream>
 
-using namespace std;
+#include <GL/glew.h>
+#include <GL/glfw3.h>
+#include <glm/glm.hpp>
 
-int main()
+static void error_callback(int error, const char* description)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    fputs(description, stderr);
 }
 
+int main(void)
+{
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+        ratio = width / (float) height;
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
