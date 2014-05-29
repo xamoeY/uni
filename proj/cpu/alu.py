a199ad12460d2b2ec18a77ea1671afea9c5bdd15
Alisa Dammer <alisa.dammer@gmail.com>
2014-05-29 13:10:11 +0200

    alu and tests fixed

--- proj/cpu/alu.py
+++ proj/cpu/alu.py
@@ -1,13 +1,5 @@
 from myhdl import *
 
-"""alu_func = {"add":      0b0000,
-            "sub":      0b0001,
-            'mul':      0b0010,
-            'inc':      0b0011,
-            'dec':      0b0100,
-            'not_a':    0b0101,
-            'b_and_a':  0b0110,
-            'a_or_b':   0b0111}"""
 
 alu_func = enum("add",
                 "sub",
@@ -15,22 +7,17 @@ alu_func = enum("add",
                 "inc",
                 "dec",
                 "not_a",
-                "b_and_a",
+                "a_and_b",
                 "a_or_b")
 
 def alu(a, b, operation, result, clk):
     """This module represents an ALU.
-    For now it can execute:
-    Arithmetic
-          add: 0000
-          sub: 0001
-          inc: 0010
-          dec: 0011 ...
-    a, b - input 32bits u_sign int
+    a, b - input 8 u_sign int
     result - output 32bits
     clk - moment, when alu works"""
     @always(clk.posedge)
     def logic():
+        print operation
         if operation == alu_func.add:
             result.next = a + b
         elif operation == alu_func.sub:
@@ -43,7 +30,7 @@ def alu(a, b, operation, result, clk):
             result.next = a - 1
         elif operation ==  alu_func.not_a:
             result.next = ~a
-        elif operation == alu_func.b_and_a:
+        elif operation == alu_func.a_and_b:
             result.next = a & b
         elif operation == alu_func.a_or_b:
             result.next = a | b
--- proj/tests/test_alu.py
+++ proj/tests/test_alu.py
@@ -6,12 +6,11 @@ def test_alu():
     a = Signal(intbv(0)[8:])
     b = Signal(intbv(0)[8:])
     result = Signal(intbv(0)[8:])
-    operation = Signal(alu_func.add)
+    operation = Signal(alu_func.sub)
     clk = Signal(bool(0))
     period = 10
 
     dut = alu(a, b, operation, result, clk)
-
     @always(delay(period))
     def clkgen():
         clk.next = not clk
@@ -19,61 +18,74 @@ def test_alu():
     @always(clk.negedge)
     def monitor():
         print "a=%s %s b=%s -> result %s" % (a, operation, b, result)
-            
+    
     @instance
-    def test_add():
-        a.next = 1
+    def stimulus():
+        # test_add
+        a.next = 5
         b.next = 2
         operation.next = alu_func.add
         yield clk.posedge
         yield clk.negedge
+        assert result == 7
+            
+        # test_sub
+        a.next = 5
+        b.next = 2
+        operation.next = alu_func.sub
+        yield clk.posedge
+        yield clk.negedge
         assert result == 3
-        raise StopSimulation
+            
+        # test_mul
+        a.next = 5
+        b.next = 5
+        operation.next = alu_func.mul
+        yield clk.posedge
+        yield clk.negedge
+        assert result == 25
 
-    """
-    @instance
-    def test_sub():
-        yield operation(7, 1, alu_func["sub"])
-        yield delay(period//4)
+        # test_inc
+        a.next = 5
+        operation.next = alu_func.inc
+        yield clk.posedge
+        yield clk.negedge
         assert result == 6
 
-    @instance
-    def test_multiply():
-        yield operation(2, 2, alu_func["mul"])
-        yield delay(period//4)
+        # test_dec
+        a.next = 5
+        operation.next = alu_func.dec
+        yield clk.posedge
+        yield clk.negedge
         assert result == 4
 
-    @instance
-    def test_increment():
-        yield operation(2, 5, alu_func["inc"])
-        yield delay(period//4)
-        assert result == 3
+        # test_not_a
+        a.next = 5
+        operation.next = alu_func.not_a
+        yield clk.posedge
+        yield clk.negedge
+        print result
+        assert result.signed() == -6
 
-    @instance
-    def test_decrement():
-        yield operation(9, 2, alu_func["dec"])
-        yield delay(period//4)
-        assert result == 8
+        # test_b_and_a
+        a.next = 5
+        b.next = 5
+        operation.next = alu_func.a_and_b
+        yield clk.posedge
+        yield clk.negedge
+        assert result == 5
 
-    @instance
-    def test_not_a():
-        yield operation(5, 1, alu_func["not_a"])
-        yield delay(period//4)
-        assert result == -6
+        # test_a_or_b
+        a.next = 5
+        b.next = 5
+        operation.next = alu_func.a_or_b
+        yield clk.posedge
+        yield clk.negedge
+        assert result == 5
 
-    @instance
-    def test_and():
-        yield operation(7, 3, alu_func["b_and_a"])
-        yield delay(period//4)
-        assert result == 3
+        raise StopSimulation
 
-    @instance
-    def test_or():
-        yield operation(5, 9, alu_func["b_or_a"])
-        yield delay(period//4)
-        assert result == 13"""
-
-    return clkgen, monitor, test_add, dut
+    return clkgen, monitor, stimulus, dut
 
 sim = Simulation(test_alu())
 sim.run()
