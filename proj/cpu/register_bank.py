ebe395bf33675ebaf953105909fd612d0504886d
Alisa Dammer <alisa.dammer@gmail.com>
2014-06-17 12:53:40 +0200

    Program loader test

--- proj/cpu/register_bank.py
+++ proj/cpu/register_bank.py
@@ -15,23 +15,23 @@ def register_bank(register, sig_in, sig_out, ovf, clk):
 	def logic():
 		if ovf == 0:
 			if register == REGISTERS["zero"]:
-				register_bank[0].next = 0
+				register_bank[0b000000].next = 0
 			elif register == REGISTERS["pc"]:
-				register_bank[1].next = sig_in
+				register_bank[0b000001].next = sig_in
 			elif register == REGISTERS["cmp_result"]:
-				register_bank[2].next = sig_in
+				register_bank[0b000010].next = sig_in
 			elif register == REGISTERS["jmp_next"]:
-				register_bank[3].next = sig_in
+				register_bank[0b000011].next = sig_in
 			elif register == REGISTERS["temp"]:
-				register_bank[4].next = sig_in
+				register_bank[0b000100].next = sig_in
 			elif register == REGISTERS["a"]:
-				register_bank[5].next = sig_in 
+				register_bank[0b000101].next = sig_in 
 			elif register == REGISTERS["b"]:
-				register_bank[6].next = sig_in
+				register_bank[0b000110].next = sig_in
 			elif register == REGISTERS["c"]:
-				register_bank[7].next = sig_in
+				register_bank[0b000111].next = sig_in
 			elif register == REGISTERS["d"]:
-				register_bank[8].next = sig_in
+				register_bank[0b001000].next = sig_in
 
 	@always_comb
 	def output():
